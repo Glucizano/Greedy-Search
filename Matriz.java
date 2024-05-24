83b8e8d13540c4611c1b3f9e65af6118abca5303
Glucizano <gabriel.lucizano@gmail.com>
2024-05-24 17:09:03 -0300

    Update Matriz.java
    Greedy Search funcional 100% comentado

--- Matriz.java
+++ Matriz.java
@@ -58,13 +58,18 @@ public class Matriz {
     // Método que vai fazer a decisão de qual cidade o Caixeiro vai em seguida
     public int[] Viagem(int[][] matriz, int atual, ArrayList<Integer>naoVisitadas, boolean ultimaCidade, int origem)
     {
+        // Booleano que será true se for a primeira iteração do loop
         boolean primeira = true;
+        // Variável que armazenara distancia que esta sendo analisada
         int analisado = 0;
+        // Variável que armazena a distância da cidade mais próxima do objetivo
         int maisProximoOrigem = 0;
+        // Vetor que armazena as duas respostas:
+        // resposta[0] = Qual cidade o caixeiro viajará
+        // resposta[1] = Qual a distância que o caixeiro percorrerá
         int[] resposta = new int[2];
-        int destino = 0;
-        int distancia = 0;
 
+        // Se for a última cidade para qual o caixeiro vai viajar, seu destino é sua cidade de origem
         if(ultimaCidade)
         {
             resposta[0] = origem;
@@ -72,33 +77,40 @@ public class Matriz {
             return resposta;
         }
 
+        // Loop em que cada iteração será uma cidade sendo analisado
         for(int x=1; x<linhas; x++)
         {
+            // A análise é feita sempre na linha da cidade de origem, visto que, a cidade de origem é seu objetivo
             analisado = matriz[origem][x];
 
+            // Se a cidade não foi visitada ainda ela é um destino valido
             if(naoVisitadas.contains(x))
             {
+                // Se for a primeira iteração do loop
                 if (primeira)
                 {
+                    // A cidade análisada será a mais próxima da origem por enquanto
                     maisProximoOrigem = analisado;
+                    // Apartir de agora as próximas iterações não serão mais a primeira
                     primeira = false;
-                    destino = x;
-                    distancia = matriz[x][atual];
+                    // O destino e a distancia que o caixeiro vai percorrer será a da primeira cidade por enquanto
+                    resposta[0] = x;
+                    resposta[1] = matriz[x][atual];
                 }
 
+                // Se a cidade analisada estiver mais perto que a cidade mais proxima do objetivo até o momento
                 if(analisado <= maisProximoOrigem)
                 {
+                    // Então a cidade sendo analisada será a nova cidade mais próxima do objetivo
                     maisProximoOrigem = analisado;
-                    destino = x;
-                    distancia = matriz[x][atual];
+                    // O valor atual do x é a cidade
+                    resposta[0] = x;
+                    // matriz[x][atual] é a distancia que o caixeiro vai percorrer para ir para a cidade analisada
+                    resposta[1] = matriz[x][atual];
                 }
             }
         }
-
-        resposta[0] = destino;
-        resposta [1] = distancia;
-
+        // Retorna a resposta com os dois valores
         return resposta;
-
     }
 }
